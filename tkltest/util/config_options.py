b5c8e4925ad5e7881edefe290b0a3b81b4f6d182
rachelt <rachelt@il.ibm.com>
2021-11-15 15:14:20 +0200

    issue #105 add gradle build option

--- tkltest/util/config_options.py
+++ tkltest/util/config_options.py
@@ -265,9 +265,9 @@ __options_spec = {
             'short_name': '-bt',
             'long_name': '--build-type',
             'type': str,
-            'choices': ['ant', 'maven'],
+            'choices': ['ant', 'maven',  'gradle'],
             'default_value': 'ant',
-            'help_message': 'build file type for compiling and running the tests - either ant or maven'
+            'help_message': 'build file type for compiling and running the tests - either ant, maven, or gradle'
         }
     },
 
