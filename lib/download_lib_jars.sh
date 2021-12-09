508a59487bfaf4e5282c9bd44f1da543ad4c42bb
Victoria-Goldin <victoria.goldin@ibm.com>
2021-12-09 10:43:22 +0200

    Added ifx-framework's class files to test/data directory

--- lib/download_lib_jars.sh
+++ lib/download_lib_jars.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
+rm -f ~/.m2/repository/org/konveyor/tackle-test-generator-core/1.0.0-SNAPSHOT/tackle-test-generator-core-1.0.0-SNAPSHOT.jar
 mvn -s ./settings.xml download:wget@get-randoop-jar download:wget@get-replacecall-jar
 mvn -s ./settings.xml dependency:copy-dependencies -DoutputDirectory=./download
