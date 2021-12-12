a365e83774f7eb84f8416ddc00d6e8999bc4e8d8
haim-kermany <haim.kermany@gmail.com>
2021-12-12 15:06:53 +0200

    check tests gradle + compare

--- test/test_generate_execute.py
+++ test/test_generate_execute.py
@@ -66,6 +66,29 @@ class GenerateExecuteTest(unittest.TestCase):
             app_info['config'] = app_config
 
 
+    def test_generate_execute_ctdamplified_combined_compare_coverage(self) -> None:
+        """Test "generate ctd-amplified" and "execute": comparing coverage reports"""
+        for app_name in self.test_list1:
+            app_info = self.test_apps[app_name]
+
+            # set up config and generate tests
+            config = app_info['config']
+            config['generate']['ctd_amplified']['base_test_generator'] = constants.BASE_TEST_GENERATORS['combined']
+            config['execute']['code_coverage'] = True
+            config['execute']['compare_to_dev_tests'] = True
+            self.__process_generate(subcommand='ctd-amplified', config=config)
+
+            # assert that expected generate resources are created
+            self.__assert_generate_resources(app_name=app_name, generate_subcmd='ctd-amplified')
+            # execute tests
+            for build_type in ['ant', 'maven', 'gradle']:
+                config['execute']['build_type'] = build_type
+                shutil.rmtree(os.path.join(constants.TKLTEST_OUTPUT_DIR_PREFIX+app_name, app_name+constants.TKLTEST_MAIN_REPORT_DIR_SUFFIX), ignore_errors=True)
+                self.__process_execute(config=config)
+
+                # assert that expected execute resources are created
+                self.__assert_execute_resources(app_name=app_name, compare_coverage=True)
+
 
     def test_generate_execute_gradle(self) -> None:
         """Test getting dependencies using gradle build"""
