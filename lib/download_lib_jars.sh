6aef1ebdb1435df75bcb75f2c463dd19cc52e9f9
Victoria Goldin <85556040+Vicky-Goldin@users.noreply.github.com>
2022-04-27 10:20:50 +0300

    Update Tackle-Test-Core jars when downloading Java library dependencies (#245)
    * implemented
    
    * updated documentation

--- lib/download_lib_jars.sh
+++ lib/download_lib_jars.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+rm -r ~/.m2/repository/org/konveyor/tackle-test-generator-unit/
+rm -r ~/.m2/repository/org/konveyor/tackle-test-generator-ui/
 mvn -s ./settings.xml -q download:wget@get-randoop-jar download:wget@get-replacecall-jar download:wget@get-evosuite-jar download:wget@get-evosuite-runtime-jar
 mvn -s ./settings.xml -q dependency:copy-dependencies -DoutputDirectory=./download
