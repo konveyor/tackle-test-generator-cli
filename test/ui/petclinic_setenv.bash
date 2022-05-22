43d26cc251144671c208ec3f2735e4e9ae2fb4c4
Saurabh Sinha <sinha108@gmail.com>
2022-05-22 10:19:18 -0400

    Updated rted default value to float (root cause for non-frag mode test generation failure where rted value was parsed as int); added petclinic test cases for generating and executing tests in non-frag mode

--- test/ui/petclinic_setenv.bash
+++ test/ui/petclinic_setenv.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PETCLINIC_CONFIG_FILE=./test/ui/data/petclinic/tkltest_ui_config.toml
+PETCLINIC_CONFIG_FILE_NONFRAG=./test/ui/data/petclinic/tkltest_ui_config_nonfrag.toml
 PETCLINIC_OUTPUT_DIR=./__tkltest-output-ui-petclinic
 PETCLINIC_CRAWL_DIR=$PETCLINIC_OUTPUT_DIR/localhost/crawl0
 PETCLINIC_CRAWLJAX_API_TEST_FILE=$PETCLINIC_CRAWL_DIR/src/test/java/generated/GeneratedTests.java
