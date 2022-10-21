79274cd0505af2659b1cf66c1bc52d1e808a57e6
Saurabh Sinha <sinha108@gmail.com>
2022-10-20 20:12:03 -0400

    Added configuration for building docker image (tkltest-unit) that support unit testing only; updated docker tests to use unit image
    Signed-off-by: Saurabh Sinha <sinha108@gmail.com>

---
+++ setup/setup_unit.py
@@ -0,0 +1,34 @@
+import tkltest
+from setuptools import setup, find_packages
+
+
+setup(
+    name='tkltest',
+    version=tkltest.__version__,
+    description='Command-line interface for generating and executing test cases on'
+                'two application versions and performing differential testing',
+    packages=find_packages(),
+    install_requires=[
+        'coverage==5.5',
+        'nose==1.3.7',
+        'PyYAML>=5.4',
+        'tabulate==0.8.9',
+        'textile==4.0.1',
+        'toml==0.10.2',
+        'yattag==1.14.0',
+        'jinja2==3.0.2',
+        'bs4==0.0.1',
+        'kaitaistruct==0.9'
+    ],
+    entry_points={
+        "console_scripts": [
+            "tkltest-unit = tkltest.tkltest_unit:main"
+        ]
+    },
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: APL-2.0',
+        'Operating System :: OS Independent'
+    ]
+)
